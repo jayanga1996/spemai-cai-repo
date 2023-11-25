915f014a385ac52240d588fda86bec243028bfff
dev-jayanga.p-at-436387679303 <jayanga.pradeepa@spemai.com>
2023-11-25 16:11:07 +0530

    second 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './styles.module.css'
 import ChatContainer from './components/ChatContainer';
-import chat_icon from './assets/Frame 5182.png'
-import chat_close_icon from '../src/assets/Group 3178.png'
+// import chat_icon from './assets/Frame 5182.png'
+// import chat_close_icon from '../src/assets/Group 3178.png'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
@@ -11,7 +11,7 @@ export const Capitalize = ({ str }) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
  export const SpemaiChatSdk =()=> {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggleChat = () => {
     setIsOpen(!isOpen);
@@ -19,7 +19,7 @@ export const Capitalize = ({ str }) => {
   return (
     <div className="chat-popup">
       <div className='icon-set'>
-      {!isOpen && (
+      {/* {!isOpen && (
       <img src={chat_icon} className='chat-icon'  onClick={toggleChat}></img>
       )}
       {isOpen && (
@@ -27,7 +27,7 @@ export const Capitalize = ({ str }) => {
         //   Chat
         // </button>
         <img src={chat_close_icon} className='chat-close-icon'  onClick={toggleChat}></img>
-      )}
+      )} */}
       </div>
      
       {isOpen && (
