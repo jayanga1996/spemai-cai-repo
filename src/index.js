d7f74882860d557f4af2acc6d7b099e28f4df5e9
dev-jayanga.p-at-436387679303 <jayanga.pradeepa@spemai.com>
2023-11-25 22:12:45 +0530

    second 8.3

--- src/index.js
+++ src/index.js
@@ -8,40 +8,7 @@ export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 export const Capitalize = ({ str }) => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
-//  export const SpemaiChatSdk =()=> {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleChat = () => {
-//     setIsOpen(!isOpen);
-//   };
-//   return (
-//     <div className="chat-popup">
-//       <div className='icon-set'>
-//       {!isOpen && (
-//       <img src={chat_icon} className='chat-icon'  onClick={toggleChat}></img>
-//       )}
-//       {isOpen && (
-//         // <button className="chat-icon" onClick={toggleChat}>
-//         //   Chat
-//         // </button>
-//         <img src={chat_close_icon} className='chat-close-icon'  onClick={toggleChat}></img>
-//       )}
-//       </div>
-     
-//       {isOpen && (
-//         <div className="chat-window">
-//           <ChatContainer />
-          
-//         </div>
-//       )}
-//     </div>
-//   );
-//}
-
-export const SpemaiCai = ({ str }) => {
-  return (
-    <div>Hello,This is SpemaiCai</div>
-  )
+  return <div>
+    How can i help you
+  </div>
 }
