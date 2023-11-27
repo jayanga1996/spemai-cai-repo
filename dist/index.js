2698875b45cb332aba9633db80c89ea0368cd9e4
dev-jayanga.p-at-436387679303 <jayanga.pradeepa@spemai.com>
2023-11-27 11:07:20 +0530

    second 8.15

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
-require("react"),exports.Capitalize=function(t){return h("div",null,h("button",null,"Submit ",{str:t.str}))};
+require("react"),
+  (exports.Capitalize = function (t) {
+    return createElement(
+      "div",
+      null,
+      createElement("button", null, "Submit ", { str: t.str })
+    );
+  });
 //# sourceMappingURL=index.js.map
